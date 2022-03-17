b6f0477969c295f1d2490c8fdd2e849bdaca01d9
Hector984 <hectoruam96@gmail.com>
2022-03-17 11:53:48 -0600

    Despliegue del proyecto

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <bounce-loader v-bind:loading="isLoading" v-bind:color="'#68d391'" v-bind:size="100"></bounce-loader>
+        <px-assets-table v-if="!isLoading" v-bind:assets="assets"></px-assets-table>
+    </div>
+</template>
+
+<script>
+import PxAssetsTable from '@/components/PxAssetsTable';
+import api from '@/api';
+
+export default {
+  name: 'PxHome',
+  components: {
+    PxAssetsTable,
+  },
+
+  data(){
+    return {
+      assets: [],
+      isLoading: false
+    }
+  },
+
+  created(){
+    this.isLoading = true;
+    api.getAssets()
+    .then(assets => (this.assets = assets))
+    .finally(() => this.isLoading = false);
+  }
+}
+</script>
